4d5d3242150f2442122782a70719ad0bf47b44b3
mynameSumin <msm4167@naver.com>
2026-06-04 13:50:37 +0900

    fix:Improve에서 화면 에러 수정

--- src/pages/landing-page/layouts/ExplainSectionLayout.tsx
+++ src/pages/landing-page/layouts/ExplainSectionLayout.tsx
@@ -12,7 +12,7 @@ export default function ExplainSectionLayout({
     <section
       className={cn(
         "flex flex-col items-center px-[20px] pt-[80px] pb-[50px] bg-[#F4F6F8] h-fit",
-        "lg:pt-[90px] lg:h-screen",
+        "lg:pt-[120px] lg:h-screen",
         "md:px-[40px] md:pt-[130px]",
         className,
       )}

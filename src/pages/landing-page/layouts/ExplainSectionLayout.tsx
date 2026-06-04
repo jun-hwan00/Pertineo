f514cd7cfeedaef9494cee7d08b4060400f8f2a1
mynameSumin <msm4167@naver.com>
2026-06-04 13:20:59 +0900

    fix: 랜딩 matrix 모바일에서는 터치 이벤트 제거

--- src/pages/landing-page/layouts/ExplainSectionLayout.tsx
+++ src/pages/landing-page/layouts/ExplainSectionLayout.tsx
@@ -11,10 +11,9 @@ export default function ExplainSectionLayout({
   return (
     <section
       className={cn(
-        "flex flex-col px-5 pt-[80px] pb-[50px] bg-[#F4F6F8] w-screen h-fit",
-        "lg:px-[120px] lg:pt-[90px] lg:h-screen",
-        "md:px-[176px] md:pt-[130px]",
-        "sm:px-[138px]",
+        "flex flex-col items-center px-[20px] pt-[80px] pb-[50px] bg-[#F4F6F8] max-w-[1200px] h-fit",
+        "lg:pt-[90px] lg:h-screen",
+        "md:px-[40px] md:pt-[130px]",
         className,
       )}
     >
